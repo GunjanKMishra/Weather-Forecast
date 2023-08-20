4457be125822cefe80ad354c0ad241242f8b0921
Gunjan Mishra <gunjanmishra096@gmail.com>
2023-08-20 11:42:49 +0530

    weather forcasting web app

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./views/*'],
+  theme: {
+    extend: {
+      fontFamily: {
+        "Bree" : ['Bree Serif', 'serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
